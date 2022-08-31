4c1a9447b7b03677f392f7b8f38eb0540b940b32
Shrabana97 <shrabanasaha97@gmail.com>
2022-08-31 15:35:53 +0530

    Fitness Logger

---
+++ Migrations/20220823054938_shivangi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FitnessTracker.Migrations
+{
+    public partial class shivangi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
